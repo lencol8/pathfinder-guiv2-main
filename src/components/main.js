cac80994b4d769fbbcbeca691e2847ce4f2a206a
AllyFocus <lenmetayer@gmail.com>
2023-07-21 10:49:49 -0700

    Initial commit

---
+++ src/components/main.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Button } from '@mui/material'
+import QuickreplyIcon from '@mui/icons-material/Quickreply'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box';
+import cheetah from '../assets/cheetah.mp4'
+import { RingLoader } from 'react-spinners'
+
+const Main = () => {
+    const [textInput, setTextInput] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [excelURL, setExcelURL] = useState({})
+
+    const handleSubmit = (e, textInput) => {
+        e.preventDefault()
+        fetch(`https://job-finder-lvija5gn3q-uc.a.run.app/api/search/?=keyword${textInput}`)
+        .then(r => r.json())
+        .then(data => setExcelURL(data))
+        console.log(textInput)
+        setLoading(loading => !loading)
+        console.log(loading)
+        console.log(excelURL)
+      }
+
+      let interval = setInterval(function(){
+        let countForVideo = document.getElementById('vid').readyState;
+        if(countForVideo === 4){
+          document.getElementById('vid').play();
+          clearInterval(interval);
+        }
+      },2000);
+
+  return (
+    <div className='main'>
+        <video id="vid" src={cheetah} autoPlay loop controls muted playsInline/>
+        <div className='content'>
+            <h1>aiOS Job Search Path Finder</h1>
+            <br></br>
+            <br></br>
+            <Box component="form" onSubmit={handleSubmit}>
+                <TextField 
+                    required
+                    onChange={e => setTextInput(e.target.value)}
+                    id="outlined-basic" label="Job Title" variant="outlined" 
+                    size="small"
+                    >
+                </TextField>
+                <br></br>
+                <Button variant="contained" 
+                type="submit"
+                endIcon={<QuickreplyIcon/>}
+                sx={{
+                    ":hover": {
+                        bgcolor: "#a9a9a9",
+                        color: "white"
+                    },
+                    backgroundColor: "#5A5A5A",
+                    mt: 2, mb: 2
+                }}>
+               Download
+                </Button>
+            </Box>
+            {!excelURL.download_url ? <RingLoader color="#00008B" loading={loading}/> : null}
+            <br></br>
+            {/* {!excelURL.download_url ? <p>Query might take a couple minutes...</p> : null} */}
+            {excelURL.download_url ? <a href={excelURL.download_url}>download here</a> : null}
+        </div>
+    </div>
+  )
+}
+
+export default Main
